5841882c1979464624144cd429b5ef64e5d91ee6
Fernando Andres Mora <ferandresjmu@hotmail.com>
2022-06-16 16:19:09 -0500

    se añade la opcion --format=c

--- script-postgres.sh
+++ script-postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 D=$(date +'%Y%m%d')
 mkdir -p /bacula/backups_prod/odooec/backup_${D}/
-PGPASSWORD="colocarpassworddeBD" pg_dump -h endpoint -U dbuser databasename > /bacula/backups_prod/odooec/backup_${D}/backup_databasename_`date +%Y%m%d_%H%M`.sql
+PGPASSWORD="colocarpassworddeBD" pg_dump --format=c -h endpoint -U dbuser databasename > /bacula/backups_prod/odooec/backup_${D}/backup_databasename_`date +%Y%m%d_%H%M`.sql
 tar -zcf /bacula/backups_prod/odooec/backup_${D}.tar.gz /bacula/backups_prod/odooec/backup_${D}
 rm -rf /bacula/backups_prod/odooec/backup_${D}
